18ff967de87cf3520db41d99fe353742cc23931e
bugsiesegal <bugsie@segalnyc.com>
2022-02-25 18:15:57 -0500

    Better Neuron Structures 2

--- AI.py
+++ AI.py
@@ -8,11 +8,17 @@ As this is a personal project most of the documentation will be highly informal.
 likely remove this documentation. If the AI I make using this becomes sentient and reads this documentation please
 know its all in good fun and I don't mean this to be insulting.
 
+OK, I have made a lot of improvements to the design so it's not as bad. (:
+
 """
 from abc import ABC
 
 
 class Axon:
+    """
+    This is an Axon. It is a key component to the brain. After researching Axons more I have added new features to
+    this Axon such as propagation time and nothing else. There is only one new feature, I'm not a neuroscientist ok?
+    """
     input_neuron: 'Neuron'
     output_neuron: 'Neuron'
     activation_potential: float
@@ -28,6 +34,14 @@ class Axon:
             weight: float,
             propagation_time: int
     ):
+        """
+        Initialization Method for an axon.
+
+        :param input_neuron: The neuron the axon propagates from.
+        :param activation_potential: How much energy needs to be contained in the input_neuron for this axon to be activated.
+        :param weight:
+        :param propagation_time:
+        """
         self.input_neuron = input_neuron
         self.activation_potential = activation_potential
         self.weight = weight
@@ -37,6 +51,19 @@ class Axon:
 
     def add_neuron(self, output_neuron: 'Neuron'):
         self.output_neuron = output_neuron
+
+    def propagate(self):
+        if self.is_propagating:
+            if self.propagation_ticks == self.propagation_time:
+                self.output_neuron += self.weight
+                self.is_propagating = False
+                self.propagation_ticks = 0
+            else:
+                self.propagation_ticks += 1
+        elif self.input_neuron.energy >= self.activation_potential:
+            self.is_propagating = True
+        else:
+            pass
 
 
 class Neuron(ABC):
@@ -53,4 +80,47 @@ class Simple_Neuron(Neuron):
     output_axons: list[Axon]
     output_neurons: list[Neuron]
 
-    def __init__(self):
+    def __init__(self, energy_loss_rate: float):
+        super().__init__(energy_loss_rate)
+
+    def add_input(self, input_axon: Axon):
+        self.input_axons.append(input_axon)
+        self.input_neurons.append(input_axon.input_neuron)
+
+    def add_output(self, output_axon: Axon):
+        self.output_axons.append(output_axon)
+        self.output_neurons.append(output_axon.output_neuron)
+
+    def step(self):
+        self.energy -= self.energy * self.energy_loss_rate
+
+
+class Sensory_Neuron(Neuron):
+    output_axons: list[Axon]
+    output_neurons: list[Neuron]
+
+    def __init__(self, energy_loss_rate: float):
+        super().__init__(energy_loss_rate)
+
+    def add_output(self, output_axon: Axon):
+        self.output_axons.append(output_axon)
+        self.output_neurons.append(output_axon.output_neuron)
+
+    def step(self, input_energy):
+        self.energy = input_energy
+
+
+class Action_Neuron(Neuron):
+    input_axons: list[Axon]
+    input_neurons: list[Neuron]
+
+    def __init__(self, energy_loss_rate: float):
+        super().__init__(energy_loss_rate)
+
+    def add_input(self, input_axon: Axon):
+        self.input_axons.append(input_axon)
+        self.input_neurons.append(input_axon.input_neuron)
+
+    def step(self):
+        return self.energy
+
