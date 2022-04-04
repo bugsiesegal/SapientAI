93f3a1fb00d559bf97d93433ee69b951034e6aae
bugsiesegal <bugsie@segalnyc.com>
2022-04-04 08:46:30 -0400

    Changes

--- Src/Axon.py
+++ Src/Axon.py
@@ -6,6 +6,8 @@ class Axon:
     Simulates Axon.
     """
     def __init__(self, input_neuron: Neuron, output_neuron: Neuron, activation_potential: float, weight: float):
+        self.gene_id = None
+
         self.input_neuron = input_neuron
         self.output_neuron = output_neuron
 
@@ -17,8 +19,10 @@ class Axon:
             self.output_neuron.energy += self.weight
 
     def to_gene(self):
-        return [self.input_neuron.gene_id, self.output_neuron.gene_id, self.activation_potential, self.weight]
+        return [self.gene_id, self.input_neuron.gene_id, self.output_neuron.gene_id, self.activation_potential, self.weight]
 
     @classmethod
     def from_gene(cls, gene: list, id_to_neuron: dict[int, Neuron]):
-        return cls(id_to_neuron[gene[0]], id_to_neuron[gene[1]], gene[2], gene[3])
+        axon = cls(id_to_neuron[gene[1]], id_to_neuron[gene[2]], gene[3], gene[4])
+        axon.gene_id = gene[0]
+        return axon
--- Src/NEAT.py
+++ Src/NEAT.py
@@ -3,21 +3,30 @@ from dataclasses import dataclass
 
 from Src.Brain import Brain
 
-from Src.params import NEURON_ENERGY_LOSS_SIGMA, NEURON_ENERGY_LOSS_RATE_PROB, AXON_ENERGY_POTENTIAL_PROB, \
-    AXON_ENERGY_POTENTIAL_SIGMA, AXON_WEIGHT_SIGMA, AXON_WEIGHT_PROB
+from Src.params import NEURON_ENERGY_LOSS_SIGMA, NEURON_ENERGY_LOSS_RATE_MUTATION_PROB, AXON_ENERGY_POTENTIAL_MUTATION_PROB, \
+    AXON_ENERGY_POTENTIAL_SIGMA, AXON_WEIGHT_SIGMA, AXON_WEIGHT_MUTATION_PROB, NEW_AXON_MUTATION_PROB
+
+def find_axon_from_connections(input_neuron: list, output_neuron: list, genome: tuple[list[list], list[list]]):
+    for axon in genome[1]:
+        if axon[1] == input_neuron[0] and axon[2] == output_neuron[0]:
+            return axon
+
 
 
 def mutate(genome: tuple[list[list], list[list]]):
-    if random.random() < NEURON_ENERGY_LOSS_RATE_PROB:
+    if random.random() < NEURON_ENERGY_LOSS_RATE_MUTATION_PROB:
         i = random.randrange(len(genome[0]))
         genome[0][i][1] = random.gauss(genome[0][i][1], NEURON_ENERGY_LOSS_SIGMA)
-    if random.random() < AXON_ENERGY_POTENTIAL_PROB:
+    if random.random() < AXON_ENERGY_POTENTIAL_MUTATION_PROB:
         i = random.randrange(len(genome[1]))
-        genome[1][i][2] = random.gauss(genome[1][i][2], AXON_ENERGY_POTENTIAL_SIGMA)
-    if random.random() < AXON_WEIGHT_PROB:
+        genome[1][i][3] = random.gauss(genome[1][i][3], AXON_ENERGY_POTENTIAL_SIGMA)
+    if random.random() < AXON_WEIGHT_MUTATION_PROB:
         i = random.randrange(len(genome[1]))
-        genome[1][i][3] = random.gauss(genome[1][i][3], AXON_WEIGHT_SIGMA)
-    if random.random() <
+        genome[1][i][4] = random.gauss(genome[1][i][4], AXON_WEIGHT_SIGMA)
+    if random.random() < NEW_AXON_MUTATION_PROB:
+        a = random.randrange(len(genome[0]))
+        b = random.randrange(len(genome[0]))
+        if a is not b and
 
 
 class Population:
